bc5f6ba921eedfeb94a4ba68a84cc05c8d9186da
troup <tim.troup@gmail.com>
2018-02-11 15:18:24 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Tiff from 'tiff.js';
+import {decode} from 'base64-arraybuffer';
+
+class Tiffnail extends React.Component {
+    render() {
+        let arraybuffer = decode(this.props.bytes);
+        let tiff = new Tiff({buffer: arraybuffer});
+        return <img src={tiff.toCanvas().toDataURL()} />;
+    }
+}
+
+Tiffnail.propTypes = {
+    bytes: PropTypes.string.isRequired
+};
