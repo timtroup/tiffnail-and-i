ae73c25f04609370297cb62d89d8f9b144045a38
troup <tim.troup@gmail.com>
2018-02-12 11:21:42 +0000

    Added support for alt and height attributes

--- src/index.js
+++ src/index.js
@@ -7,12 +7,16 @@ class Tiffnail extends React.Component {
     render() {
         let arraybuffer = decode(this.props.bytes);
         let tiff = new Tiff({buffer: arraybuffer});
-        return <img src={tiff.toCanvas().toDataURL()} />;
+        return <img src={tiff.toCanvas().toDataURL()}
+                    alt={this.props.alt ? this.props.alt:"undefined"}
+                    height={this.props.height ? this.props.height:"100%"} />;
     }
 }
 
 Tiffnail.propTypes = {
-    bytes: PropTypes.string.isRequired
+    bytes: PropTypes.string.isRequired,
+    alt: PropTypes.string,
+    height: PropTypes.string
 };
 
 export default Tiffnail;
